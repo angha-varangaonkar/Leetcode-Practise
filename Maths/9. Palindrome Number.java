13d6878b5c66c9ffcd79207d8f1e606ac1ccefd8
Angha <anghavarangaonkar1104@gmail.com>
2025-01-11 13:27:27 +0530

    day1

---
+++ Maths/9. Palindrome Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false; // for negative numbers bcz negative numbers are not palimdrone number
+        }
+
+        int n=x;   //taakin n ko compare kar sake
+        int revNum=0;
+
+        while(n>0){
+            int d=n%10;  //last digit nikalegi
+
+            revNum=revNum*10+d; //reverse number nikalega 
+
+            n=n/10;  // n ko reduce karna matlab 121 se 12 and then 1 
+
+        }
+
+        if(revNum==x){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+}
